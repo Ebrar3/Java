e5a1b82831ad4336e8067bf4248c4d9b02913632
Ebrar <151376689+Ebrar3@users.noreply.github.com>
2024-09-05 15:03:53 +0300

    first commit
    eskiler yüklendi

---
+++ variables/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Java case sensitive bi dildir yani büyük- küçük harf duyarlılığı var
+        //Camel casing= ilk kelimenin harfi kğçğk diğerlerinin ilk harfi büyük şekilde bitişik yazılması (deve hörgücü gibi)
+        System.out.println("öğrenci sayım");
+        int ogrenciSayisi = 10 ;
+        String mesaj = "öğrenci sayısı : ";
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +(ogrenciSayisi+1));
+
+    }
+}
